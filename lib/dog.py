f657fa9519f57c568b26f3e41acb41e8ce8855cd
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-10 10:08:55 -0500

    lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,43 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Mastiff"):
+        self._name = name
+        self._breed = breed
+        if not self._is_valid_name(name):
+            print("Name must be string between 1 and 25 characters.")
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if self._is_valid_name(value):
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+   
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+    
+
+    def _is_valid_name(self,value):
+        return isinstance(value, str) and 1 <= len(value) <= 25
+    
+
+my_dog = Dog("Fido", "Mastiff")
+print(my_dog.name)  
+
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,38 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Katie", job = "Legal"):
+        self.name = name
+        self.job = job
+        if not self._is_valid_name(name):
+            print( "Name must be string between 1 and 25 characters.")
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if self._is_valid_name(value):
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+    def _is_valid_name(self,value):
+        return isinstance(value,str) and 1 <= len(value) <= 25
+    
+my_self = Person("Katie", "Legal")
+print(my_self.name)
